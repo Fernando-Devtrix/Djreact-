08d71f0158209539b200af4e61789c8f24450b45
Fernando <fernando_0970@outlook.es>
2020-05-03 15:08:38 -0500

    1: Starting app, small api created and its called from react

---
+++ backend/djangoreact_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/fernando/760AB7240AB6E073/Django/djreact/backend/djangoreact_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
